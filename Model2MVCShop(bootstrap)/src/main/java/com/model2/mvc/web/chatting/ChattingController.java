f001e1d2b297c6a0d9949c59d73fcd5ca72e909e
user <user@Bitcamp>
2017-09-22 13:58:10 +0900

    second

--- Model2MVCShop(bootstrap)/src/main/java/com/model2/mvc/web/chatting/ChattingController.java
+++ Model2MVCShop(bootstrap)/src/main/java/com/model2/mvc/web/chatting/ChattingController.java
@@ -23,7 +23,7 @@ import com.model2.mvc.service.domain.User;
 import com.model2.mvc.service.product.ProductService;
 import com.model2.mvc.service.user.UserService;
 
-
+//test 중
 //==> 회원관리 Controller
 @Controller
 @RequestMapping("/chatting/*")
